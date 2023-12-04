76711f820e44ffc596a2209431b5f0f5b535ede0
douglasferreirag <douglas.ferreirag@gmail.com>
2023-12-03 22:22:39 -0300

    Banco de dados

--- src/main/java/com/webflux/pokedex/PokedexApplication.java
+++ src/main/java/com/webflux/pokedex/PokedexApplication.java
@@ -1,7 +1,13 @@
 package com.webflux.pokedex;
 
+import com.webflux.pokedex.model.Pokemon;
+import com.webflux.pokedex.repository.PokemonRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.ReactiveMongoOperations;
+import reactor.core.publisher.Flux;
 
 @SpringBootApplication
 public class PokedexApplication {
@@ -10,7 +16,30 @@ public class PokedexApplication {
 
 		SpringApplication.run(PokedexApplication.class, args);
 
-		System.out.println("Hello World com webflux");
+
+
+	}
+
+	@Bean
+	CommandLineRunner init (ReactiveMongoOperations operations, PokemonRepository repository)
+	{
+
+		return args -> {
+
+			Flux<Pokemon> pokemonFlux = Flux.just(
+
+								new Pokemon(null, "Blastoise", "Marisco", "Torrent", 9.0 ),
+								new Pokemon(null, "Caterpie", "Minhoca", "Poeira do Escudo", 2.09),
+								new Pokemon(null, "Bulbassauro", "Semente", "OverGrow", 6.09))
+						.flatMap(repository::save);
+
+				pokemonFlux
+						.thenMany(repository.findAll())
+						.subscribe(System.out::println);
+
+
+
+			};
 
 	}
 
